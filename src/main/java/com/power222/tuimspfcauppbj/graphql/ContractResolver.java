d03f9356d1bd468d28bd3fba62c3eb1b124e20af
Andrei Belkin <andrei.belkin@pm.me>
2021-09-20 04:56:46 -0400

    GQ05 : Méthode dans résolveur pour entité StudentApplication (aussi, refactoring et fix de méthode résolveur pour Contract)

--- src/main/java/com/power222/tuimspfcauppbj/graphql/ContractResolver.java
+++ src/main/java/com/power222/tuimspfcauppbj/graphql/ContractResolver.java
@@ -1,16 +1,81 @@
 package com.power222.tuimspfcauppbj.graphql;
 
+import com.power222.tuimspfcauppbj.model.Contract;
+import com.power222.tuimspfcauppbj.model.StudentApplication;
+import com.power222.tuimspfcauppbj.service.ContractService;
+import com.power222.tuimspfcauppbj.service.StudentApplicationService;
 import com.power222.tuimspfcauppbj.util.ContractSignatureState;
+import com.power222.tuimspfcauppbj.util.StudentApplicationState;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class ContractResolver implements GraphQLQueryResolver {
+    private final ContractService contractService;
+    private final StudentApplicationService studentApplicationService;
+
+    public ContractResolver(ContractService contractService, StudentApplicationService studentApplicationService) {
+        this.contractService = contractService;
+        this.studentApplicationService = studentApplicationService;
+    }
+
     public ContractSchema contract(long id) {
-        return new ContractSchema(id, "file ici", "eng college ici", "eng compagnie ici", "eng étudiant ici", 37.5F, "reasonForRejection ici", ContractSignatureState.SIGNED, 0L, 0L, 0L, 0L);
+        Optional<Contract> contractOpt = contractService.getContractById(id);
+        if (contractOpt.isEmpty())
+            return null;
+        Contract contract = contractOpt.get();
+
+        return new ContractSchema(
+                id,
+                contract.getFile(),
+                contract.getEngagementCollege(),
+                contract.getEngagementCompany(),
+                contract.getEngagementStudent(),
+                contract.getTotalHoursPerWeek(),
+                contract.getReasonForRejection(),
+                contract.getSignatureState(),
+                studentApplication(contract.getStudentApplication() != null ? contract.getStudentApplication().getId() : -1L),
+                contract.getInternEvaluation() != null ? contract.getInternEvaluation().getId() : -1L,
+                contract.getAdmin() != null ? contract.getAdmin().getId() : -1L,
+                contract.getBusinessEvaluation() != null ? contract.getBusinessEvaluation().getId() : -1L
+        );
+    }
+
+    public StudentApplicationSchema studentApplication(long id) {
+        Optional<StudentApplication> studentApplicationOpt = studentApplicationService.getApplicationById(id);
+        if (studentApplicationOpt.isEmpty())
+            return null;
+        StudentApplication studentApplication = studentApplicationOpt.get();
+
+        return new StudentApplicationSchema(
+                id,
+                studentApplication.getState(),
+                studentApplication.getReasonForRejection(),
+                studentApplication.getOffer() != null ? studentApplication.getOffer().getId() : -1L,
+                studentApplication.getStudent() != null ? studentApplication.getStudent().getId() : -1L,
+                studentApplication.getResume() != null ? studentApplication.getResume().getId() : -1L,
+                studentApplication.getInterview() != null ? studentApplication.getInterview().getId() : -1L,
+                studentApplication.getContract() != null ? studentApplication.getContract().getId() : -1L
+        );
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class StudentApplicationSchema {
+        private long id;
+        private StudentApplicationState state;
+        private String reasonForRejection;
+        private long offerId;
+        private long studentId;
+        private long resumeId;
+        private long interviewId;
+        private long contractId;
     }
 
     @Data
@@ -25,8 +90,8 @@ public class ContractResolver implements GraphQLQueryResolver {
         private float totalHoursPerWeek;
         private String reasonForRejection;
         private ContractSignatureState signatureState;
-        private long studentApplicationId;
-        private long interEvaluationId;
+        private StudentApplicationSchema studentApplication;
+        private long internEvaluationId;
         private long adminId;
         private long businessEvaluationId;
     }
